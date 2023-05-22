265fca399b59028ef1c700320e432c1da5eaa514
doaamhmd1012001 <doaa1012001@gmail.com>
2023-05-22 23:42:28 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,71 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(stepcounter_testv2());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final _streamSubscriptions = <StreamSubscription<dynamic>>[];
 
-  // This widget is the root of your application.
+StreamSubscription? _accelerometerStreamSubscription;
+
+int stepCounter = 0;
+double previousvalue = 0.0;
+bool _isStepUp = false;
+
+class stepcounter_testv2 extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<stepcounter_testv2> createState() => _stepcounter_testv2State();
+}
+@override
+void initState() {
+  _initAccelerometer();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+@override
+void dispose() {
+  _stopAccelerometer();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+void _initAccelerometer() {
+  _accelerometerStreamSubscription = accelerometerEvents.listen((AccelerometerEvent event) {
+    double y = event.y;
+    double magnitude = sqrt(event.x * event.x + event.y * event.y + event.z * event.z);
+    double delta = magnitude - previousvalue;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+    if (delta > 2.0 && !_isStepUp && magnitude > 11.0) {
+      stepCounter;
+      _isStepUp = true;
+        stepCounter = stepCounter;
+    } else if (delta < -2.0) {
+      _isStepUp = false;
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            team_a_points++;
-                          });
-                        },
-                        child: Text('Add 1 Point',style: TextStyle(fontSize: 17),),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 42, 145, 209),
-                            foregroundColor: Color.fromARGB(255, 255, 255, 255),
-                            minimumSize: Size(130, 40)),
-                      ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    previousvalue = magnitude;
+  });
 }
+
+void _stopAccelerometer() {
+  _accelerometerStreamSubscription?.cancel();
+  _accelerometerStreamSubscription = null;
+}
+
+class _stepcounter_testv2State extends State<stepcounter_testv2> {
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Step Counter'),
+    ),
+    body: Center(
+      child: Text(
+        'the step counter for the user: $stepCounter',style: TextStyle(fontSize: 30),
+      ),
+    ),
+  );
+}
+
+  }
+
